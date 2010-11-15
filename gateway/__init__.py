f03964b6080afb1431f7b7776ab4ab639b6836c9
Ivan Willig <iwillig@gmail.com>
2010-11-15 15:09:51 -0500

    added auth stuff to the main configuration

--- gateway/__init__.py
+++ gateway/__init__.py
@@ -1,4 +1,7 @@
 from pyramid_beaker import session_factory_from_settings
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+
 
 def main(global_config, **settings):
     """ This function returns a Pylons WSGI application.
@@ -6,6 +9,7 @@ def main(global_config, **settings):
     from paste.deploy.converters import asbool
     from pyramid.configuration import Configurator
     from gateway.models import initialize_sql
+    from gateway.security import groupfinder
     from gateway.messaging import process_messages
 
     db_string = settings.get('db_string')
@@ -13,7 +17,14 @@ def main(global_config, **settings):
         raise ValueError("No 'db_string' value in application "
                          "configuration.")
     initialize_sql(db_string, asbool(settings.get('db_echo')))
-    config = Configurator(settings=settings)
+    authn_policy = AuthTktAuthenticationPolicy(
+        'sosecret', callback=groupfinder)
+    authz_policy = ACLAuthorizationPolicy()
+
+    config = Configurator(settings=settings,
+                          root_factory='gateway.models.RootFactory',
+                          authentication_policy=authn_policy,
+                          authorization_policy=authz_policy)
     config.begin()
     session_factory = session_factory_from_settings(settings)
     config.set_session_factory(session_factory)
@@ -21,10 +32,13 @@ def main(global_config, **settings):
         'static',
         'gateway:static/'
         )
+    config.add_handler('dashboard','/',
+                       'gateway.handlers:Dashboard',
+                       action='index')
     config.add_handler('main','/:action',
                       handler='gateway.handlers:Dashboard')
-    config.add_handler('dashboard','/',
-                       'gateway.handlers:Dashboard',action='index')
+    config.add_handler('users','user/:action',
+                      handler='gateway.handlers:UserHandler')
     config.add_handler('meter','meter/:action/:id',
                        handler='gateway.handlers:MeterHandler') 
     config.add_handler('circuit','circuit/:action/:id',
