5bf04daffd32929f0516a63687e9f7b0b89a3f7b
Ivan Willig <ivan@mars.(none)>
2010-12-08 14:26:03 -0500

    Added models for handling the meter messages.

---
+++ gateway/jobs.py
@@ -0,0 +1,88 @@
+from gateway.models import Job, SystemLog, PrimaryLog, OutgoingMessage
+from gateway.utils import make_message
+
+def valid(test, against):
+    for key in against:
+        if key not in test:
+            return False
+    return True
+
+def make_delete(message, session): 
+    job = session.query(Job).get(message["jobid"])
+    if job:
+        circuit = job.circuit
+        job.state = False
+        session.merge(job)
+        if message["cr"]:
+            circuit.credit = message["cr"]
+            session.merge(circuit)
+    else: 
+        session.add(SystemLog(
+                "Unable to find job message %s " % message))
+
+def make_pp(message, circuit, session): 
+    if valid(message.keys(),
+             ['status', 'cid', 'tu', 'mid', 'wh', 'job']):
+        log = PrimaryLog(circuit=circuit,
+                         watthours=message["wh"],
+                         use_time=message["tu"],
+                         credit=message.get("cr"),
+                         status=int(message["status"]))
+        # override the credit and status value from the meter.
+        circuit.credit = log.credit
+        circuit.status = log.status
+        session.add(log)
+        session.merge(circuit)
+    else:
+        session.add(
+            SystemLog(text="Unable to process message %s"
+                      % message.uuid))
+
+
+def make_nocw(message, circuit, session): 
+    msg = OutgoingMessage(
+            circuit.account.phone,
+            make_message("nocw-alert.txt",
+                         lang=circuit.account.lang,
+                         account=circuit.pin))
+    session.add(msg)
+    session.flush()
+    session.add(
+        SystemLog(
+            "Low credit alert for circuit %s sent to %s" % (circuit.pin,
+                                                            msg.number)))
+
+def make_lcw(message, circuit, session): 
+    msg = OutgoingMessage(circuit.account.phone,
+                          make_message("lcw-alert.txt",
+                                       lang=circuit.account.lang,
+                                       account=circuit.pin))
+    session.add(msg)
+    # send email 
+
+def make_md(message, circuit, session): 
+    log = SystemLog(
+        "Meter %s just falied, please investagte." % circuit.meter.name)
+    session.add(log)
+    # send email 
+
+def make_ce(message, circuit, session): 
+    log = SystemLog(
+        "Component %s just failed, please investagate." % circuit.pin)
+    session.add(log) 
+    # send email 
+     
+def make_pmax(message, circuit, session): 
+    msg = OutgoingMessage(circuit.account.phone,
+                          make_message("power-max-alert.txt",
+                                       lang=circuit.account.lang,
+                                       account=circuit.pin))
+    session.add(msg) 
+
+
+def make_emax(message, circuit, session): 
+    pass 
+
+
+def make_sdc(message,circuit,session):
+    pass 
