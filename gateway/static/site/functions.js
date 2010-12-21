a448793e659ab58d4782b6f2cf72bc3afb204277
Ivan Willig <iwillig@gmail.com>
2010-12-21 10:31:05 -0500

    New global functions file.

---
+++ gateway/static/site/functions.js
@@ -0,0 +1,32 @@
+/* 
+ * Global functions
+ * 
+ */ 
+
+(function ($) {
+
+  function SetWidget(widget) {
+    $(this).sortable({
+      connectWith: this
+    });
+
+    $( widget ).addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" ).find( ".widget-header" )
+      .addClass( "ui-widget-header ui-corner-all" )
+      .prepend( "<span class='ui-icon ui-icon-minusthick'></span>")
+      .end()
+      .find( ".widget-content" );
+    $( ".widget-header .ui-icon" ).click(function() {
+          $( this ).toggleClass( "ui-icon-minusthick" ).
+              toggleClass( "ui-icon-plusthick" );
+          $( this ).parents( ".widget:first" ).
+              find( ".widget-content" ).toggle();
+    });
+ 
+//    $(this).disableSelection();
+
+}
+
+    $.fn.setWidget = SetWidget;
+
+})(jQuery); 
+  
