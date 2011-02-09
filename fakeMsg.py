2b792aa4a84748808e86575630e3d2e71b7a5002
Ivan Willig <ivan@cuba.(none)>
2011-02-09 07:07:09 +0300

    Removed simplejson requirements from the testing script.

--- fakeMsg.py
+++ fakeMsg.py
@@ -1,17 +1,18 @@
 import uuid
+from datetime import datetime
 import urllib2
-import simplejson
+import urllib
 
 def make_request(msg,phone="18182124554"): 
+    data = urllib.urlencode({ "message" : msg,
+                              "number" : phone,
+                              "uuid" : str(uuid.uuid4()) })
     return urllib2.Request(
-        data=simplejson.dumps({ "text" : msg,
-                                "from" : phone,
-                                "uuid" : str(uuid.uuid4()) } 
-                              ),url="http://localhost:6543/sms/send")
+        data=data,url="http://localhost:6543/interface/send/8")
 
 def test_consumer_messages():
     account = "ijr495"
-    token = 78782523959
+    token = 38902754073
 
     response = urllib2.urlopen(make_request("bal." + account))
     print("----------------------------") 
@@ -75,17 +76,17 @@ def test_consumer_messages():
     print(response.read())
     print("----------------------------") 
 
-    # response = urllib2.urlopen(make_request("use." + account)) 
-    # print("----------------------------") 
-    # print("testing use en") 
-    # print(response.read())
-    # print("----------------------------") 
+    response = urllib2.urlopen(make_request("use." + account)) 
+    print("----------------------------") 
+    print("testing use en") 
+    print(response.read())
+    print("----------------------------") 
 
-    # response = urllib2.urlopen(make_request("conso." + account)) 
-    # print("----------------------------") 
-    # print("testing use fr") 
-    # print(response.read())
-    # print("----------------------------") 
+    response = urllib2.urlopen(make_request("conso." + account)) 
+    print("----------------------------") 
+    print("testing use fr") 
+    print(response.read())
+    print("----------------------------") 
 
 
     response = urllib2.urlopen(make_request("this should fail" + account)) 
@@ -102,7 +103,7 @@ def test_meter_messages():
     meter_name = "demo001"
 #    test primary log 
     response = urllib2.urlopen(
-        make_request("(job=pp&cid=" + cid  + "&mid=" + meter_name + "&wh=10.00&tu=12.12&cr=20.10&status=1)",phone="18185846103"))
+        make_request("(job=pp&cid=" + cid  + "&mid=" + meter_name + "&wh=10.00&tu=12.12&ts=20110107192318&cr=20.10&status=1)",phone="18185846103"))
     print("----------------------------") 
     print("testing primary log") 
     print(response.read())
@@ -116,48 +117,48 @@ def test_meter_messages():
     print("----------------------------") 
 
 
-    # response = urllib2.urlopen(
-    #     make_request("(job=alerts&mid=" + 
-    #                  meter_name + "&cid=" + cid + "&alert=md)",phone="18185846103")) 
-    # print("----------------------------") 
-    # print("testing alert meter down") 
-    # print(response.read())
-    # print("----------------------------") 
-    # # test meter sd card not found 
-    # response = urllib2.urlopen(
-    #     make_request("(job=alerts&mid=" + 
-    #                  meter_name + "&cid=" + cid + "&alert=sdc)",phone="18185846103")) 
-    # print("----------------------------") 
-    # print("testing alert test meter sd card not found") 
-    # print(response.read())
-    # print("----------------------------") 
-    # # test circuit low credit
-    # response = urllib2.urlopen(
-    #     make_request("(job=alerts&mid=" + 
-    #                  meter_name + "&cid=" + cid 
-    #                  + "&alert=lcw&cr=10.00)",phone="18185846103")) 
-    # print("----------------------------") 
-    # print("testing alert low credit") 
-    # print(response.read())
-    # print("----------------------------") 
+    response = urllib2.urlopen(
+        make_request("(job=alerts&mid=" + 
+                     meter_name + "&cid=" + cid + "&alert=md)",phone="18185846103")) 
+    print("----------------------------") 
+    print("testing alert meter down") 
+    print(response.read())
+    print("----------------------------") 
+    # test meter sd card not found 
+    response = urllib2.urlopen(
+        make_request("(job=alerts&mid=" + 
+                     meter_name + "&cid=" + cid + "&alert=sdc)",phone="18185846103")) 
+    print("----------------------------") 
+    print("testing alert test meter sd card not found") 
+    print(response.read())
+    print("----------------------------") 
+    # test circuit low credit
+    response = urllib2.urlopen(
+        make_request("(job=alerts&mid=" + 
+                     meter_name + "&cid=" + cid 
+                     + "&alert=lcw&cr=10.00)",phone="18185846103")) 
+    print("----------------------------") 
+    print("testing alert low credit") 
+    print(response.read())
+    print("----------------------------") 
 
-    # # test circuit no credit 
-    # response = urllib2.urlopen(
-    #     make_request("(job=alerts&mid=" 
-    #                  + meter_name + "&cid=" 
-    #                  + cid + "&alert=nocw&cr=00.00)",phone="18185846103")) 
-    # print("----------------------------") 
-    # print("testing alert no credit") 
-    # print(response.read())
-    # print("----------------------------") 
-    # # test circuit compontent failure 
-    # response = urllib2.urlopen(
-    #     make_request("(job=alerts&mid=" 
-    #                  + meter_name + "&cid=" + cid + "&alert=ce)",phone="18185846103")) 
-    # print("----------------------------") 
-    # print("testing alert ce") 
-    # print(response.read())
-    # print("----------------------------") 
+    # test circuit no credit 
+    response = urllib2.urlopen(
+        make_request("(job=alerts&mid=" 
+                     + meter_name + "&cid=" 
+                     + cid + "&alert=nocw&cr=00.00)",phone="18185846103")) 
+    print("----------------------------") 
+    print("testing alert no credit") 
+    print(response.read())
+    print("----------------------------") 
+    # test circuit compontent failure 
+    response = urllib2.urlopen(
+        make_request("(job=alerts&mid=" 
+                     + meter_name + "&cid=" + cid + "&alert=ce)",phone="18185846103")) 
+    print("----------------------------") 
+    print("testing alert ce") 
+    print(response.read())
+    print("----------------------------") 
 
-#test_consumer_messages()
-test_meter_messages()
+test_consumer_messages()
+#test_meter_messages()
